6a7b65e8e37ca116eba7bbc72a901d4aff8dda90
Ryan Bower <rbower@qti.qualcomm.com>
2025-11-04 23:44:51 -0800

    chore: add remaining markings
    Signed-off-by: Ryan Bower <rbower@qti.qualcomm.com>

--- packages/common/css-utils/src/cli/config.ts
+++ packages/common/css-utils/src/cli/config.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+
 import {cosmiconfig, type CosmiconfigResult} from "cosmiconfig"
 
 export async function loadConfig(configPath?: string) {
