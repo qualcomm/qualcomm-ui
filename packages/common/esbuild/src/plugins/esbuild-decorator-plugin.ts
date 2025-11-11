81d362c8e125b57dffe44829a64e8dd77c1a0ea5
Ryan Bower <rbower@qti.qualcomm.com>
2025-11-10 20:56:46 -0800

    fix: re-add license headers
    Signed-off-by: Ryan Bower <rbower@qti.qualcomm.com>

--- packages/common/esbuild/src/plugins/esbuild-decorator-plugin.ts
+++ packages/common/esbuild/src/plugins/esbuild-decorator-plugin.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+
 import type {Plugin} from "esbuild"
 import {readFile} from "node:fs/promises"
 import {dirname, join} from "node:path"
--- packages/common/esbuild/src/utils.ts
+++ packages/common/esbuild/src/utils.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+
 import {readdir} from "node:fs/promises"
 
 export function getArg(argv: any[], key: string): string | undefined {
