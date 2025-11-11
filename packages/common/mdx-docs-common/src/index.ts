fac8ce61858629a5157d8f91d41e94a1caca64d0
Ryan Bower <rbower@qti.qualcomm.com>
2025-11-10 21:35:00 -0800

    test
    Signed-off-by: Ryan Bower <rbower@qti.qualcomm.com>

--- packages/common/mdx-docs-common/src/index.ts
+++ packages/common/mdx-docs-common/src/index.ts
@@ -1,6 +1,6 @@
-// add for lint test
 export type * from "./demo-plugin.types"
 export type * from "./docs-plugin.types"
+// eslint-disable-next-line
 export * from "./exports"
 export * from "./id-service"
 export * from "./search"
