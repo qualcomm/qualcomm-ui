27c3c20fd0241d0fc67d2d07e6813a48a70daa3a
Ryan Bower <rbower@qti.qualcomm.com>
2025-11-10 21:13:17 -0800

    chore: add license headers
    Signed-off-by: Ryan Bower <rbower@qti.qualcomm.com>

--- packages/common/typedoc-common/src/utils.ts
+++ packages/common/typedoc-common/src/utils.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+
 import type {QuiPropDeclaration} from "./types"
 
 export function humanFileSize(size: number) {
