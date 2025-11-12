e143a67a6b0ab9e814806ee189e4755108904102
Ryan Bower <rbower@qti.qualcomm.com>
2025-11-11 23:55:13 -0800

    fix: table docs start
    Signed-off-by: Ryan Bower <rbower@qti.qualcomm.com>

--- packages/docs/react-table-docs/vite.config.ts
+++ packages/docs/react-table-docs/vite.config.ts
@@ -1,6 +1,8 @@
 import mdx from "@mdx-js/rollup"
 import {reactRouter} from "@react-router/dev/vite"
 import tailwindcss from "@tailwindcss/vite"
+import {dirname, resolve} from "node:path"
+import {fileURLToPath} from "node:url"
 import {defineConfig} from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 
@@ -10,6 +12,10 @@ import {
   quiDocsPlugin,
   reactDemoPlugin,
 } from "@qualcomm-ui/mdx-vite"
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const root = resolve(__dirname, ".")
 
 export default defineConfig({
   optimizeDeps: {
@@ -46,6 +52,24 @@ export default defineConfig({
       demoPattern: "src/routes/**/*/demos/*.tsx",
     }),
   ],
+  resolve: {
+    alias: [
+      {
+        find: "@atlaskit/pragmatic-drag-and-drop",
+        replacement: resolve(
+          root,
+          "./node_modules/@atlaskit/pragmatic-drag-and-drop/dist/esm/entry-point",
+        ),
+      },
+      {
+        find: "@atlaskit/pragmatic-drag-and-drop-hitbox",
+        replacement: resolve(
+          root,
+          "./node_modules/@atlaskit/pragmatic-drag-and-drop-hitbox/dist/esm",
+        ),
+      },
+    ],
+  },
   server: {
     port: process.env.PORT ? parseInt(process.env.PORT) : 3200,
     warmup: {
