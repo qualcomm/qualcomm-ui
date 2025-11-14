87e26773532d3700b48538d705c4abf890c3cb25
Ryan Bower <rbower@qti.qualcomm.com>
2025-11-13 22:33:10 -0800

    perf: use simple css transition on mobile
    Signed-off-by: Ryan Bower <rbower@qti.qualcomm.com>

--- packages/frameworks/react-router-utils/src/client/theme/use-correct-css-transition.ts
+++ packages/frameworks/react-router-utils/src/client/theme/use-correct-css-transition.ts
@@ -3,6 +3,8 @@
 
 import {type MouseEvent, useCallback} from "react"
 
+import UAParser from "my-ua-parser"
+
 function getDocument(): Document | undefined {
   return typeof document === "undefined" ? undefined : document
 }
@@ -47,28 +49,35 @@ export function useCorrectCssTransition({
         })
       } else {
         if (event) {
-          const x = event.clientX
-          const y = event.clientY
-          const endRadius = Math.hypot(
-            Math.max(x, innerWidth - x),
-            Math.max(y, innerHeight - y),
-          )
+          if (typeof window !== "undefined") {
+            const userAgent = new UAParser(window.navigator.userAgent)
+            const os = userAgent.getOS()
+            // Only enable view transitions on desktop browsers.
+            if (os.name && !["Android", "iOS"].includes(os.name)) {
+              const x = event.clientX
+              const y = event.clientY
+              const endRadius = Math.hypot(
+                Math.max(x, innerWidth - x),
+                Math.max(y, innerHeight - y),
+              )
 
-          document.documentElement.style.setProperty(
-            "--view-transition-x",
-            `${x}px`,
-          )
-          document.documentElement.style.setProperty(
-            "--view-transition-y",
-            `${y}px`,
-          )
-          document.documentElement.style.setProperty(
-            "--view-transition-r",
-            `${endRadius}px`,
-          )
+              document.documentElement.style.setProperty(
+                "--view-transition-x",
+                `${x}px`,
+              )
+              document.documentElement.style.setProperty(
+                "--view-transition-y",
+                `${y}px`,
+              )
+              document.documentElement.style.setProperty(
+                "--view-transition-r",
+                `${endRadius}px`,
+              )
+              document.documentElement.style.viewTransitionName =
+                "theme-transition"
+            }
+          }
         }
-
-        document.documentElement.style.viewTransitionName = "theme-transition"
 
         const transition = document.startViewTransition(() => {
           callback()
@@ -76,6 +85,9 @@ export function useCorrectCssTransition({
 
         transition.finished.finally(() => {
           document.documentElement.style.viewTransitionName = ""
+          document.documentElement.style.removeProperty("--view-transition-x")
+          document.documentElement.style.removeProperty("--view-transition-y")
+          document.documentElement.style.removeProperty("--view-transition-r")
         })
       }
     },
