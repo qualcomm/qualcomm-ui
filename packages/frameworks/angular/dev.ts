237b447daedbb46c68f3bc9ee74acc8402076236
Ryan Bower <rbower@qti.qualcomm.com>
2025-11-16 22:24:26 -0800

    improved angular dev script
    Signed-off-by: Ryan Bower <rbower@qti.qualcomm.com>

--- packages/frameworks/angular/dev.ts
+++ packages/frameworks/angular/dev.ts
@@ -5,6 +5,7 @@
 
 import chokidar from "chokidar"
 import {execSync} from "node:child_process"
+import {access, readdir} from "node:fs/promises"
 import {dirname, resolve} from "node:path"
 import {setTimeout as setTimeoutPromise} from "node:timers/promises"
 import {fileURLToPath} from "node:url"
@@ -12,41 +13,106 @@ import {fileURLToPath} from "node:url"
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const DEBOUNCE_MS = 2000
-let timeoutId: NodeJS.Timeout | null = null
+const ANGULAR_CORE_DIST_DIR = resolve(__dirname, "../angular-core/dist")
+const ANGULAR_CORE_SRC_DIR = resolve(__dirname, "../angular-core/src")
 
-/**
- * Closes the watcher and starts the build process in watch mode.
- */
-function startProcess() {
-  console.debug("@qualcomm-ui/angular-core output stable, starting build")
-  watcher.close()
-  execSync("pnpm build --watch", {stdio: "inherit"})
+let timeoutId: ReturnType<typeof setTimeout> | null = null
+
+// give the current angular-core dist dir time to clear its contents
+await setTimeoutPromise(500)
+
+async function scanForEntrypoints() {
+  const files = await readdir(ANGULAR_CORE_SRC_DIR, {withFileTypes: true})
+
+  const entrypoints = (
+    await Promise.all(
+      files
+        .filter((file) => file.isDirectory())
+        .map(async (file) => {
+          const path = resolve(
+            ANGULAR_CORE_SRC_DIR,
+            file.name,
+            "ng-package.json",
+          )
+          return access(path)
+            .then(() => file.name)
+            .catch(() => null)
+        }),
+    )
+  ).filter((name): name is string => name !== null)
+
+  return entrypoints.map((name) => resolve(ANGULAR_CORE_DIST_DIR, name))
 }
 
-await setTimeoutPromise(DEBOUNCE_MS)
+async function validateAngularCore() {
+  const entrypoints = await scanForEntrypoints()
 
-const watcher = chokidar.watch("dist", {
-  awaitWriteFinish: {
-    pollInterval: 100,
-    stabilityThreshold: 1000,
-  },
-  cwd: resolve(__dirname, "../angular-core"),
-  persistent: true,
-})
+  const builtEntrypoints = await Promise.all(
+    entrypoints.map((entrypoint) =>
+      access(resolve(entrypoint, "index.d.ts"))
+        .then(() => true)
+        .catch(() => null),
+    ),
+  )
 
-watcher.on("all", () => {
-  if (timeoutId) {
-    clearTimeout(timeoutId)
+  return builtEntrypoints.every(Boolean)
+}
+
+async function waitForAngularCore() {
+  console.debug("Waiting for angular-core")
+  let i = 0
+  let angularCoreBuilt = false
+
+  while (i < 60 && !angularCoreBuilt) {
+    i++
+
+    angularCoreBuilt = await validateAngularCore()
+    if (angularCoreBuilt) {
+      console.debug(
+        "@qualcomm-ui/angular-core built, starting watcher for @qualcomm-ui/angular",
+      )
+      execSync("pnpm build --watch", {stdio: "inherit"})
+    } else {
+      await setTimeoutPromise(1000)
+    }
+  }
+}
+
+await waitForAngularCore()
+
+// TODO: enhance with restart whenever a new entrypoint is added
+function _startWatcher() {
+  /**
+   * Closes the watcher and starts the build process in watch mode.
+   */
+  function startProcess() {
+    console.debug("@qualcomm-ui/angular-core output stable, starting build")
+    watcher.close()
+    execSync("pnpm build --watch", {stdio: "inherit"})
   }
 
-  timeoutId = setTimeout(() => {
-    startProcess()
-    timeoutId = null
-  }, DEBOUNCE_MS)
-})
+  const watcher = chokidar.watch(ANGULAR_CORE_DIST_DIR, {
+    awaitWriteFinish: {
+      pollInterval: 100,
+      stabilityThreshold: 1000,
+    },
+    persistent: true,
+  })
 
-watcher.on("error", (error) => {
-  console.error("Watcher error:", error)
-})
+  watcher.on("all", () => {
+    if (timeoutId) {
+      clearTimeout(timeoutId)
+    }
 
-console.log("Watching for changes...")
+    timeoutId = setTimeout(() => {
+      startProcess()
+      timeoutId = null
+    }, DEBOUNCE_MS)
+  })
+
+  watcher.on("error", (error) => {
+    console.error("Watcher error:", error)
+  })
+
+  console.log("Watching for changes...")
+}
