ea041e4ac23bc7c32a3b55d1fb3f7c711c15a083
Ryan Bower <rbower@qti.qualcomm.com>
2025-11-17 12:54:32 -0800

    add license headers
    Signed-off-by: Ryan Bower <rbower@qti.qualcomm.com>

--- packages/frameworks/angular/src/transitions/qui-preload.directive.ts
+++ packages/frameworks/angular/src/transitions/qui-preload.directive.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+
 import {type AfterViewInit, Directive, signal} from "@angular/core"
 
 import {useCsrCheck} from "@qualcomm-ui/angular-core/common"
