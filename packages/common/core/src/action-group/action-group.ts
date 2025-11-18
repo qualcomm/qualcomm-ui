63b4689fe4795a9aac300d3c97c4d3e7f3474006
Ryan Bower <rbower@qti.qualcomm.com>
2025-11-18 00:23:27 -0800

    refactor: deprecate core action-group api (unused)
    Signed-off-by: Ryan Bower <rbower@qti.qualcomm.com>

--- packages/common/core/src/action-group/action-group.ts
+++ packages/common/core/src/action-group/action-group.ts
@@ -1,11 +1,17 @@
 // Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
 // SPDX-License-Identifier: BSD-3-Clause-Clear
 
+/**
+ * @deprecated
+ */
 export interface ActionGroupRootBindings {
   "data-part": "root"
   "data-scope": "action-group"
 }
 
+/**
+ * @deprecated
+ */
 export function getActionGroupRootBindings(): ActionGroupRootBindings {
   return {
     "data-part": "root",
