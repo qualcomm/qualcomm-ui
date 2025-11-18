b1e5d2ca6ae664848f66139bd7cfe3a2f233b4ba
Ryan Bower <rbower@qti.qualcomm.com>
2025-11-18 00:24:11 -0800

    fix(table-pagination): add missing provider
    Signed-off-by: Ryan Bower <rbower@qti.qualcomm.com>

--- packages/frameworks/angular/src/table/components/table-pagination.directive.ts
+++ packages/frameworks/angular/src/table/components/table-pagination.directive.ts
@@ -4,10 +4,12 @@
 import {computed, Directive} from "@angular/core"
 
 import {PaginationRootDirective} from "@qualcomm-ui/angular/pagination"
+import {providePaginationContext} from "@qualcomm-ui/angular-core/pagination"
 
 import {qdsTableApi} from "./qds-table-api"
 
 @Directive({
+  providers: [providePaginationContext()],
   selector: "[q-table-pagination]",
   standalone: false,
 })
