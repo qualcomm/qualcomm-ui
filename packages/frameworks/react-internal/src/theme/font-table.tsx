c9ac74c8674e6b4350159d9d23c79abf3c435af5
Ryan Bower <rbower@qti.qualcomm.com>
2025-11-21 01:48:15 -0800

    licenses fixed
    Signed-off-by: Ryan Bower <rbower@qti.qualcomm.com>

--- packages/frameworks/react-internal/src/theme/font-table.tsx
+++ packages/frameworks/react-internal/src/theme/font-table.tsx
@@ -1,3 +1,6 @@
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+
 import {useEffect, useState} from "react"
 
 import type {FontData} from "./theme-fonts"
