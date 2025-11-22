f2b89dd012a1f04e241cccb77e21f0e7a88ede9d
Ryan Bower <rbower@qti.qualcomm.com>
2025-11-21 17:04:52 -0800

    add license headers
    Signed-off-by: Ryan Bower <rbower@qti.qualcomm.com>

--- packages/frameworks/react-internal/src/layout/doc-link.tsx
+++ packages/frameworks/react-internal/src/layout/doc-link.tsx
@@ -1,3 +1,6 @@
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+
 import type {ComponentPropsWithRef} from "react"
 
 import {Link} from "react-router"
