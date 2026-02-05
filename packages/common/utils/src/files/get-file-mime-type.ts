b5d11863c8e7c263e5aff4157925e0f148755d12
Dustin Pham <dustinpham95@gmail.com>
2026-02-04 18:50:10 -0800

    add license info for file-upload
    Signed-off-by: Dustin Pham <dustinpham95@gmail.com>

--- packages/common/utils/src/files/get-file-mime-type.ts
+++ packages/common/utils/src/files/get-file-mime-type.ts
@@ -1,3 +1,8 @@
+// Modified from https://github.com/chakra-ui/zag
+// MIT License
+// Changes from Qualcomm Technologies, Inc. are provided under the following license:
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: BSD-3-Clause-Clear
 import {mimeTypesMap} from "./mime-types"
 import type {FileMimeType} from "./types"
 
